9feee357fd656149f45620abc09a706a6e0db037
sundas saleem <sundass986@gmail.com>
2023-06-01 11:49:19 +0500

    feat:edit comment

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+//i am progrramer.
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
